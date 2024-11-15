582f326b994dc0ef3c6ebf5d88ae8882f53b12b6
rutracrafter <artur.melo.rodrigues@gmail.com>
2024-11-15 00:13:25 -0800

    completed exercise1-13

--- chapter1/exercise1-13/histVertical.c
+++ chapter1/exercise1-13/histVertical.c
@@ -45,17 +45,26 @@ int main() {
 	}
 
 	for (int i = 0; i < 12; i++) {
-		wordLengths[i] = 12 * ((wordLengths[i] - min) / (max - min));
+		if (max != min) {
+			wordLengths[i] = 12 * ((wordLengths[i] - min) / (max - min));
+		} else {
+			wordLengths[i] = 0;
+		}
+	
 	}	
 
 	for (int i = 0; i < 12; i++) {
-		printf("%2.0f letters: ", (float) i);
-		for (int j = 0; j < (int) wordLengths[i]; j++) {
-			printf("*");	
+		for (int j = 0; j < 12; j++) {
+			if (wordLengths[j] >= (12 - i)) {
+				printf(" * ");
+			}
+			else {
+				printf("   ");
+			}
 		}
-		printf("\n");	
-	}
-	
+		printf("\n");
+	} 
+
 	printf("Total number of words: %d\n", wc);
 	printf("Max frequency: %.0f\n", max);
 	printf("Min frequency: %.0f\n", min);
